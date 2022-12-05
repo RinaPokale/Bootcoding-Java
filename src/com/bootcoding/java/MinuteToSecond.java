268895df3e607bb8f892ff3b9c97af73ac5abc1f
Rina Pokale <rinapokale009@gmail.com>
2022-12-05 09:54:43 +0530

    program to convert minutes into seconds

---
+++ src/com/bootcoding/java/MinuteToSecond.java
@@ -0,0 +1,12 @@
+package com.bootcoding.java;
+
+public class MinuteToSecond {
+    public static int convert(int minutes){
+        return minutes * 60;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(convert(10));
+    }
+}
+
