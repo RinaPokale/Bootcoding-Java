4b10c96c6a1bfde232a79d158de921ec6a4a0a00
Rina Pokale <rinapokale009@gmail.com>
2023-01-12 02:10:24 +0530

    print all numbers from 1 to 100, whose remainder will be 3 after divided by specified number.

---
+++ src/com/bootcoding/java/Assignment/PrintDivisibleBySpecifiedNum.java
@@ -0,0 +1,17 @@
+package com.bootcoding.java.Assignment;
+
+public class PrintDivisibleBySpecifiedNum {
+        public static void printNumDivBySpecifiedNum(int n){
+            for(int i = 1; i <= 100; i++){
+                if(i % n == 3){
+                    System.out.println(i);
+                }
+            }
+        }
+        public static void main(String[] args) {
+            printNumDivBySpecifiedNum(15);
+//        printNumDivBySpecifiedNum(25);
+        }
+}
+
+
