ee991845978e4ae7b362728a56d706b8f2f96300
Rina Pokale <rinapokale009@gmail.com>
2023-01-11 09:40:28 +0530

    program to convert minutes into seconds

---
+++ src/com/bootcoding/java/Assignment/ProductOfNum.java
@@ -0,0 +1,11 @@
+package com.bootcoding.java.Assignment;
+
+public class ProductOfNum {
+    public static int productOfTwoNum(int n1, int n2){
+        return n1 * n2;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(productOfTwoNum(12, 34));
+    }
+}
