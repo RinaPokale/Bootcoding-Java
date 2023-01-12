1e94b59116418a3df448283854d0b7fb51aaed0a
Rina Pokale <rinapokale009@gmail.com>
2023-01-12 08:51:40 +0530

    print Square of array elements using method

---
+++ src/com/bootcoding/java/Assignment/SquareOfArrayElements.java
@@ -0,0 +1,21 @@
+package com.bootcoding.java.Assignment;
+
+public class SquareOfArrayElements {
+    public int[] squareOfArrayElements(int[] nums){
+        int[] SquareOfNum = new int[nums.length];
+        for(int i = 0; i < nums.length; i++){
+            SquareOfNum[i] = nums[i] * nums[i];
+        }
+        return SquareOfNum;
+    }
+    public static void main(String[] args) {
+        SquareOfArrayElements sq = new SquareOfArrayElements();
+
+        int[] nums = {1, 2, 3, 4, 5};
+        int[] sqNum = sq.squareOfArrayElements(nums);
+
+        for(int i = 0; i < sqNum.length; i++){
+            System.out.println("Square of " + nums[i] + " = " +sqNum[i]);
+        }
+    }
+}
