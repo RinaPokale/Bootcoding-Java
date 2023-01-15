98f2f83f3a0a8a240d6713efbdbb809251763ac5
Rina Pokale <rinapokale009@gmail.com>
2023-01-15 20:44:28 +0530

    program to display the pattern of right angle triangle with a number which will repeat a number in a row.

--- src/com/bootcoding/java/patterns/TrianPattRepeatNum.java
+++ src/com/bootcoding/java/patterns/TrianPattRepeatNum.java
@@ -1,8 +1,13 @@
 package com.bootcoding.java.patterns;
 
 public class TrianPattRepeatNum {
+    public static void main(String[] args) {
+        trianglePatternRepeatNum(4);
+        System.out.println();
+        trianglePatternRepeatNum(6);
+    }
 
-    public static void trianglePatternRepeatNum(int n){
+    private static void trianglePatternRepeatNum(int n){
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= i; j++){
                 System.out.print(i + " ");
@@ -11,10 +16,5 @@ public class TrianPattRepeatNum {
         }
     }
 
-    public static void main(String[] args) {
-        trianglePatternRepeatNum(4);
-        System.out.println();
-        trianglePatternRepeatNum(6);
 
-    }
 }
