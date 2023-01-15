95e40b485fa7501a2a9720a283e56fa5820f78e1
Rina Pokale <rinapokale009@gmail.com>
2023-01-15 20:26:32 +0530

    program to display the pattern like right angle triangle using an asterisk

--- src/com/bootcoding/java/patterns/TrianglePatternStar.java
+++ src/com/bootcoding/java/patterns/TrianglePatternStar.java
@@ -1,6 +1,10 @@
 package com.bootcoding.java.patterns;
 
 public class TrianglePatternStar {
+    public static void main(String[] args) {
+        trianglePatternStar(5);
+    }
+
     public static void trianglePatternStar(int n){
         for(int i = 0; i < n; i++){
             for (int j = 0; j <= i; j++){
@@ -10,8 +14,5 @@ public class TrianglePatternStar {
         }
     }
 
-    public static void main(String[] args) {
-        trianglePatternStar(5);
-    }
 
 }
