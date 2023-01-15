69d784111f424545011d4d901fe7e833d591d14d
Rina Pokale <rinapokale009@gmail.com>
2023-01-15 20:31:49 +0530

    program to display the pattern of right angle triangle with continuous number

--- src/com/bootcoding/java/patterns/TrianPattContinuousNum.java
+++ src/com/bootcoding/java/patterns/TrianPattContinuousNum.java
@@ -1,6 +1,11 @@
 package com.bootcoding.java.patterns;
 
 public class TrianPattContinuousNum {
+
+    public static void main(String[] args) {
+        triangleContinuousNumPatt(4);
+    }
+
     public static void triangleContinuousNumPatt(int n){
         int count = 0;
         for(int i = 0; i < n; i++){
@@ -10,8 +15,5 @@ public class TrianPattContinuousNum {
             }
             System.out.println();
         }
-    }
-    public static void main(String[] args) {
-        triangleContinuousNumPatt(4);
     }
 }
