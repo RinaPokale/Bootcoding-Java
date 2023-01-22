7c695b6c400507f46a76e29aed96c22e4e2374c8
Rina Pokale <rinapokale009@gmail.com>
2023-01-22 18:31:35 +0530

    Program to print the pyramid of even numbers

---
+++ src/com/bootcoding/java/patterns/PyramidWithEvenNum.java
@@ -0,0 +1,25 @@
+package com.bootcoding.java.patterns;
+
+public class PyramidWithEvenNum {
+    private static void printEvenNum(int n){
+        int num = 1;
+        for(int i = 1; i <= n; i++){
+            //spaces
+            for(int j = 1; j <= n-i; j++){
+                System.out.print(" ");
+            }
+
+            //pattern
+            for(int k = 1; k <=i; k++){
+                System.out.print(2*num + " ");
+                num++;
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        printEvenNum(4);
+    }
+
+}
